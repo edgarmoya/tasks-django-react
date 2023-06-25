e3d70f2810388efd3c3dff5841e25b69dc92266f
edgarmoya <edgarcmoya@gmail.com>
2023-06-25 02:07:35 -0400

    Implement search functionality with clear button

--- frontend/src/components/TasksList.jsx
+++ frontend/src/components/TasksList.jsx
@@ -1,27 +1,12 @@
-import { React, useEffect, useState } from "react";
+import React from "react";
 import TaskCard from "./TaskCard";
-import { getAllTasks } from "../api/tasks.api";
 
-function TasksList() {
-  const [tasks, setTasks] = useState([]);
-
-  useEffect(() => {
-    async function loadTask() {
-      const res = await getAllTasks();
-      setTasks(res.data);
-    }
-    loadTask();
-  }, []);
-
-  const handleDeleteTask = async (taskId) => {
-    setTasks(tasks.filter((task) => task.id !== taskId));
-  };
-
+function TasksList({ tasks, onTaskDeleted }) {
   return (
     <div className="row my-4">
       {tasks.map((task) => (
         <div key={task.id} className="col-lg-4 col-md-6 col-sm-12 mb-4 d-flex">
-          <TaskCard task={task} onTaskDeleted={handleDeleteTask} />
+          <TaskCard task={task} onTaskDeleted={onTaskDeleted} />
         </div>
       ))}
     </div>
--- frontend/src/pages/TasksPage.jsx
+++ frontend/src/pages/TasksPage.jsx
@@ -1,10 +1,36 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import TasksList from "../components/TasksList";
+import { getAllTasks } from "../api/tasks.api";
+import Navbar from "../components/Navbar";
 
 function TasksPage() {
+  const [tasks, setTasks] = useState([]);
+  const [filteredTasks, setFilteredTasks] = useState([]);
+
+  useEffect(() => {
+    async function loadTask() {
+      const res = await getAllTasks();
+      setTasks(res.data);
+      setFilteredTasks(res.data);
+    }
+    loadTask();
+  }, []);
+
+  const handleSearch = (searchValue) => {
+    const filtered = tasks.filter((task) =>
+      task.title.toLowerCase().includes(searchValue.toLowerCase())
+    );
+    setFilteredTasks(filtered);
+  };
+
+  const handleDeleteTask = async (taskId) => {
+    setTasks(tasks.filter((task) => task.id !== taskId));
+  };
+
   return (
     <React.Fragment>
-      <TasksList />
+      <Navbar onSearch={handleSearch} onClear={() => setFilteredTasks(tasks)} />
+      <TasksList tasks={filteredTasks} onTaskDeleted={handleDeleteTask} />
     </React.Fragment>
   );
 }
